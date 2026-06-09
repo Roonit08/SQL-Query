cb04ec7d332a223682b0b83e0fa7b775d01b8b32
Roonit08 <ronitbhattarai77@gmail.com>
2026-06-09 08:29:14 +0545

    updated the ddl basics file

--- DDL basics.sql
+++ DDL basics.sql
@@ -1,3 +1,4 @@
+
 SELECT setting
 FROM pg_settings
 WHERE NAME = 'data_directory';
@@ -6,7 +7,7 @@ SELECT oid, datname
 FROM pg_database;
 
 -- Creating databse --
-CREATE DATABASE Ronit;
+CREATE DATABASE Ronit;  
 
 -- creating a table--
 
@@ -16,15 +17,28 @@ brand VARCHAR(50),
 club_name VARCHAR(50)
 );
 
+-- display table
 SELECT * FROM kit;
 
+-- adding a new column
 ALTER TABLE jersey ADD COLUMN quality VARCHAR(50);
 
+-- change column datatype
+ALTER TABLE jersey ALTER COLUMN price TYPE float;
+
+-- 
 -- drop a column
 ALTER TABLE jersey DROP COLUMN quality;
 
 -- rename table 
 ALTER TABLE jersey RENAME TO kit;
 
+-- rename column
+ALTER TABLE kit RENAME COLUMN club_name TO team_name;
 
+-- DROP TABLE jersey
 
+-- Truncate table
+TRUNCATE TABLE jersey;
+
+SELECT * FROM kit;
